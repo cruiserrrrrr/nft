d0930344937530f74b0e422659a7d74d4e7b2fe7
cruiserrrrrr <90287011+cruiserrrrrr@users.noreply.github.com>
2022-01-28 00:25:45 +0300

    test
    test

--- scripts/nft.js
+++ scripts/nft.js
@@ -62,106 +62,25 @@ document.getElementsByClassName("btn__bids")[0].onclick = () => {
 
 
 
-// (() => {
+	document.getElementById('search__icon').onclick = function() {
+	document.getElementById('header').classList.add('header__none');	
 
-//     const properties = {
-//         spaceDiameter  : 32,    //диаметр пространства точки
-//         dotDiameter    : 14,    //диаметр самой точки
-//         wavelength     : 100,   //длинна волны
-//         velocity       : .4,   //скорость по умолчанию .02
-//         direction      : 1,     //направление (1: из центра, -1: в центр)
-//         displacement   : 1      //смещение (0: отключить, 1: включить)
-//     }
+	document.getElementById('mobile__search__act').classList.add('search__active__mob');
+	}
+	
+	document.getElementById('header__search').onclick = function() {
+	document.getElementById('header').classList.add('header__none');	
+		
+	document.getElementById('search').classList.add('search__active__mob');
 
-//     const canvas = document.createElement('canvas');
-//     const ctx = canvas.getContext('2d');                    //получаем контекст
+	}
 
-//     let w = canvas.width = innerWidth;                      //ширина равна ширине области просмотра
-//     let h = canvas.height = innerHeight;                    //высота равна высоте области просмотра
 
-//     let dotsList;                                           //переменная для хранения списка точек
 
-//     canvas.style.background = 'rgba(17, 17, 23 ,1)';        //цвет canvas
-//     document.querySelector('body').appendChild(canvas);     //добавляем canvas в body
+	// const btn = document.getElementById('close__popup')
+	// btn.onclick = function() {
 
-//     window.onresize = function() {                          //обновляем размеры Canvas если меняется размер окна
-//         w = canvas.width = innerWidth;
-//         h = canvas.height = innerHeight;
-//         init();
-//     }
-
-//     class Dot {                                             //конструктор точки
-//         constructor(x, y, num) {
-//             this.x = x;
-//             this.y = y;
-//             this.radius = properties.dotDiameter / 2;
-//             this.scale = getDistance(x, y) / properties.wavelength;
-//             this.text = num;
-//         }
-
-//         update() {
-//             this.resize();
-//             this.draw();
-//         }
-
-//         resize() {
-//             this.scale = this.scale - properties.velocity * properties.direction;
-//         }
-
-//         draw() {
-
-//             let s = ( 1 - Math.abs(Math.sin(this.scale)));
-//             let o = (1 - s) * 255;
-//             let r = this.radius * s;
-            
-//             ctx.beginPath();
-//             ctx.arc(this.x, this.y, r, 0, 2 * Math.PI, false);
-//             ctx.closePath();
-//             ctx.fillStyle = 'rgba( 255,'+ o +', '+ o +', '+ s +')';
-//             ctx.fill();
-//             // ctx.fillText(this.text, this.x, this.y);
-//         }
-//     }
-
-//     init();
-//     function init() {
-//         dotsList = [];
-
-//         const dotsCountX = w / properties.spaceDiameter | 0; //Math.floor(w / properties.spaceDiameter);
-//         const dotsCountY = h / properties.spaceDiameter | 0;
-//         const startX = (properties.spaceDiameter + w - dotsCountX * properties.spaceDiameter) / 2;
-//         const startY = (properties.spaceDiameter + h - dotsCountY * properties.spaceDiameter) / 2;
-
-//         let displacement = properties.spaceDiameter / 4 * properties.displacement;
-
-//         for ( let j = 0; j < dotsCountY; j++) {
-//             displacement = - displacement;
-//             let y = startY + j * properties.spaceDiameter;
-//             for (let i = 0; i < dotsCountX; i++) {
-//                 let x = startX + i * properties.spaceDiameter + displacement;
-//                 dotsList.push(new Dot(x, y, j + i));
-//             }
-//         }
-//     }
-
-//     loop();
-//     function loop() {
-//         ctx.clearRect(0, 0, w, h);
-
-//         for (let a in dotsList) {
-//             dotsList[a].update();
-//         }
-
-//         requestAnimationFrame(loop);            //зацикливаем loop();
-//     }
-
-//     function getDistance(x, y) {                    //      получаем расстояние от центра области просмотра до точки ...
-//         let dx = w / 2 - x;                         //  ... используя теорему Пифагора
-//         let dy = h / 2 - y;                         //      https://www.youtube.com/watch?v=MX6xpzsTfpM
-//         return Math.sqrt((dx * dx) + (dy * dy));    //
-//     }
-
-// })();
+	// document.getElementById('ggit_popup').classList.remove("popup_open");
 
 
 
